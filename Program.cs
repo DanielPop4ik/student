fccbe5774c852d204d46114237d315ee9bcb2256
John Doe <johndoe@example.com>
2023-02-02 11:15:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число");
+
+string? numberStr = Console.ReadLine();
+if (numberStr != null){
+    int number = Math.Abs(int.Parse(numberStr));
+    Console.WriteLine(number%10);
+}
